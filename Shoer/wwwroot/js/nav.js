b9f98615885f5141da2e80aa908a0f9be00b35ea
Emre KAYACIK <emrekayacik@eskisehir.edu.tr>
2021-06-04 21:34:38 +0300

    stylesheet and front-end beautified

---
+++ Shoer/wwwroot/js/nav.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(() => {
+    $('#navbar-main').show();
+});
+
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+        $('#navbar-main').hide("slow");
+
+    } else {
+        $('#navbar-main').show("slow");
+    }
+    lastScrollTop = st;
+});
+
+goRegister = () => {
+    window.location.href = $('#urlRegister').val();
+};
+goList = () => {
+    window.location.href = $('#urlList').val();
+};
